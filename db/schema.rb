1d4cbeef09117d417c40c60e317a9d475f494c3d
keighty <k80.leonard@gmail.com>
2013-10-07 16:06:11 -0700

    adds secure password to users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131007164242) do
+ActiveRecord::Schema.define(version: 20131007165028) do
 
   create_table "comments", force: true do |t|
     t.integer  "post_id"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20131007164242) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
