0b1fb6aafefc9fc411051b200f1abe5197c50b7d
keighty <k80.leonard@gmail.com>
2013-10-07 09:46:41 -0700

    adds user validations and index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131006213846) do
+ActiveRecord::Schema.define(version: 20131007164242) do
 
   create_table "comments", force: true do |t|
     t.integer  "post_id"
@@ -42,5 +42,7 @@ ActiveRecord::Schema.define(version: 20131006213846) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
