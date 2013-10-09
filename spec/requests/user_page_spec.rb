bdc58e2b0e0dc1144e48ba23220c63da99098a77
keighty <k80.leonard@gmail.com>
2013-10-09 11:49:29 -0700

    adds specs for testing authentication

--- spec/requests/user_page_spec.rb
+++ spec/requests/user_page_spec.rb
@@ -59,13 +59,38 @@ describe "User pages" do
     end
   end
 
+  describe "signin" do
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+    end
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
+
+      it { should have_title(user.name) }
+      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Sign out',    href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
+    end
+  end
+
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit edit_user_path(user) }
 
     describe "page" do
       it { should have_content("Update your profile") }
-      it { should have_title("Edit user") }
+      it { should have_title("Edit") }
     end
 
     describe "with invalid information" do
