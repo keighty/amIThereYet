9d9407ad4fb46f44a40d19fec1d0272f7cec132c
keighty <k80.leonard@gmail.com>
2013-10-09 11:59:44 -0700

    added update actions and flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,16 +35,12 @@ class UsersController < ApplicationController
   end
 
   # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      flash[:success] = "Your profile has been updated"
+      redirect_to @user
+    else
+      render action: 'edit'
     end
   end
 
