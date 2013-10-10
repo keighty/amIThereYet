edba3ace6ece8ea51c1528a06268c5ebb0f707da
keighty <k80.leonard@gmail.com>
2013-10-10 11:27:09 -0700

    adds goal views and partials

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,20 @@
 class GoalsController < ApplicationController
+
+  def show
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @goal = @user.goals.create!(goal_params)
+
+    respond_to do |format|
+      format.html { redirect_to @user, notice: 'New goal created' }
+      format.js
+    end
+  end
+
+  private
+    def goal_params
+      params.require(:goal).permit(:description, :motivation)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131008194653) do
+ActiveRecord::Schema.define(version: 20131010173935) do
 
   create_table "comments", force: true do |t|
     t.integer  "post_id"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 20131008194653) do
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+
+  create_table "goals", force: true do |t|
+    t.integer  "user_id"
+    t.string   "description"
+    t.text     "motivation"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "goals", ["user_id"], name: "index_goals_on_user_id"
 
   create_table "posts", force: true do |t|
     t.string   "title"
