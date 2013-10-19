27e1325d8ce9ec66468890c1b1c23b9f0296ec93
keighty <k80.leonard@gmail.com>
2013-10-19 12:24:48 -0700

    removed autogen factories folder

--- spec/factories/goals.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :goal do
-    user nil
-    description "MyString"
-    motivation "MyText"
-  end
-end
