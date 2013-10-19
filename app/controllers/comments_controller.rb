11f93f35662dea5af5b6aea2d318b24c2c7fc132
keighty <k80.leonard@gmail.com>
2013-10-19 11:55:40 -0700

    adds user id to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,14 @@
 class CommentsController < ApplicationController
+  before_action :set_user
+  # before_action :signed_in_user, only: [:create]
+
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create!(comment_params)
+    @comment = @post.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
 
     respond_to do |format|
-      format.html { redirect_to @post, notice: 'Comment posted' }
       format.js
     end
   end
@@ -12,6 +16,17 @@ class CommentsController < ApplicationController
   private
     def comment_params
       params.require(:comment).permit(:body, :user_id)
+    end
+
+    def set_user
+      @user = current_user
+    end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
     end
 
 end
