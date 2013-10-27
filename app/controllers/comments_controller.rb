abdb3f673dcc3e85cc7fe3a12d6bf7ea41bae1bf
keighty <k80.leonard@gmail.com>
2013-10-27 13:17:09 -0700

    adds before filters and delete scaffold

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,9 @@
 class CommentsController < ApplicationController
   before_action :set_user
-  # before_action :signed_in_user, only: [:create]
+  before_action :set_post
+  before_action :signed_in_user, only: [:create]
 
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
     @comment.user_id = current_user.id
     @comment.save
@@ -13,6 +13,10 @@ class CommentsController < ApplicationController
     end
   end
 
+  def delete
+
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:body, :user_id)
@@ -20,6 +24,10 @@ class CommentsController < ApplicationController
 
     def set_user
       @user = current_user
+    end
+
+    def set_post
+      @post = Post.find(params[:post_id])
     end
 
     def signed_in_user
