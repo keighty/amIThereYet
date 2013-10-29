fb099398a1bd4743b4f5081ceb3bbf3c09eaa9c6
keighty <k80.leonard@gmail.com>
2013-10-29 15:14:15 -0700

    modifies hours migration

--- db/migrate/20131021171237_create_hours.rb
+++ db/migrate/20131021171237_create_hours.rb
@@ -2,7 +2,7 @@ class CreateHours < ActiveRecord::Migration
   def change
     create_table :hours do |t|
       t.references :goal, index: true
-      t.time :duration
+      t.float :duration
       t.string :category
       t.string :description
 
--- db/migrate/20131029195618_change_duration_to_number_for_hours.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDurationToNumberForHours < ActiveRecord::Migration
-  def change
-    change_column :hours, :duration, 'decimal USING CAST(duration AS decimal)'
-
-  end
-end
